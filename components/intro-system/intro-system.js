db585641565dcfe2d3d5a0522b87892883b714c5
Felipe Genuino <design@felipegenuino.com>
2014-09-30 04:17:42 -0300

    update commit

--- components/intro-system/intro-system.js
+++ components/intro-system/intro-system.js
@@ -1,3 +1,145 @@
   $('.intro-system-carousel ').carousel({
     interval: false
   })
+
+
+
+
+
+/* 'CONTROLES DAS ANIMAÇÕES 
+========================================================================================== */
+
+//Slides inicial
+
+		 	$('.intro-system-slide0 .animation-1').addClass(' animated fadeInUpBig'); 
+			$('.intro-system-slide0 .animation-2').addClass('delay-1s animated fadeInUp'); 
+			$('.intro-system-slide0 .animation-3').addClass('delay-2s animated fadeInUp'); 
+			$('.intro-system-slide0 .animation-4').addClass('delay-3s animated fadeInUp'); 
+			$('.intro-system-slide0 .animation-5').addClass('delay-4s animated fadeInUp'); 
+			$('.intro-system-slide0 .animation-6').addClass('delay-5s animated fadeInUp'); 
+			$('.intro-system-slide0 .animation-7').addClass('delay-6s animated fadeInUp'); 
+
+ 
+//necessidade | concorrencia | resultado de troca
+$( ".intro-system-button-to-slide-1" ).click(function() {
+			$('.intro-system-slide1 .animation-1').addClass(' animated fadeInUp'); 
+
+			$('.intro-system-slide1 .animation-2').addClass('delay-1s animated fadeInUp'); 
+		 	$('.intro-system-slide1 .animation-3').addClass('delay-2s animated fadeInUp'); 
+			$('.intro-system-slide1 .animation-4').addClass('delay-2s animated fadeInUp'); 
+			
+			$('.intro-system-slide1 .animation-5').addClass('delay-3s animated fadeInUp'); 
+		 	$('.intro-system-slide1 .animation-6').addClass('delay-4s animated fadeInUp'); 
+			$('.intro-system-slide1 .animation-7').addClass('delay-4s animated fadeInUp'); 
+
+			$('.intro-system-slide1 .animation-8').addClass(' delay-5s animated fadeInUp'); 
+		 	$('.intro-system-slide1 .animation-9').addClass(' delay-6s animated fadeInUp'); 
+			$('.intro-system-slide1 .animation-10').addClass('delay-6s animated fadeInUp'); 
+ 
+});
+
+
+
+//fatores qualitativos e quantitativos
+$( ".intro-system-button-to-slide-2" ).click(function() {
+			$('.intro-system-slide2 .animation-1').addClass(' animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-2').addClass('delay-1s animated fadeInUp'); 
+
+			$('.intro-system-slide2 .animation-3').addClass(' delay-2s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-4').addClass(' delay-2s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-5').addClass(' delay-3s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-6').addClass(' delay-4s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-7').addClass(' delay-5s animated fadeInUp'); 
+
+			$('.intro-system-slide2 .animation-8').addClass(' delay-6s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-9').addClass(' delay-6s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-10').addClass('delay-7s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-11').addClass('delay-8s animated fadeInUp'); 
+			$('.intro-system-slide2 .animation-12').addClass('delay-9s animated fadeInUp'); 
+});
+
+
+
+
+ 
+
+//o que importar...
+$( ".intro-system-button-to-slide-3" ).click(function() {
+			$('.intro-system-slide3 .animation-1').addClass(' animated fadeInUp'); 
+			$('.intro-system-slide3 .animation-2').addClass(' delay-1s animated fadeInUp'); 
+			$('.intro-system-slide3 .animation-3').addClass(' delay-2s animated fadeInUp'); 
+
+			$('.intro-system-slide3 .animation-4').addClass(' delay-3s animated fadeInUp'); 
+			$('.intro-system-slide3 .animation-5').addClass(' delay-4s animated fadeInUp'); 
+			$('.intro-system-slide3 .animation-6').addClass(' delay-5s animated fadeInUp'); 
+			$('.intro-system-slide3 .animation-7').addClass(' delay-6s animated fadeInUp'); 
+			$('.intro-system-slide3 .animation-8').addClass(' delay-7s animated fadeInUp'); 
+});
+
+
+
+
+
+//Teste de potencial mercadológico...
+$( ".intro-system-button-to-slide-4" ).click(function() {
+			$('.intro-system-slide4 .animation-1').addClass(' animated fadeInUp'); 
+			$('.intro-system-slide4 .animation-2').addClass(' delay-1s animated fadeInUp'); 
+			$('.intro-system-slide4 .animation-3').addClass(' delay-2s animated fadeInUp'); 
+			$('.intro-system-slide4 .animation-4').addClass(' delay-3s animated fadeInUp'); 
+			$('.intro-system-slide4 .animation-5').addClass(' delay-4s animated fadeInUp'); 
+			$('.intro-system-slide4 .animation-6').addClass(' delay-5s animated fadeInRightBig'); 
+});
+
+
+
+
+//Como importar...
+$( ".intro-system-button-to-slide-5" ).click(function() {
+			$('.intro-system-slide5 .animation-1').addClass(' animated fadeInUp'); 
+			$('.intro-system-slide5 .animation-2').addClass(' delay-1s animated fadeInUp'); 
+			$('.intro-system-slide5 .animation-3').addClass(' delay-2s animated fadeInUp'); 
+			$('.intro-system-slide5 .animation-4').addClass(' delay-3s animated fadeInUp'); 
+			$('.intro-system-slide5 .animation-5').addClass(' delay-4s animated fadeInUp'); 
+			$('.intro-system-slide5 .animation-6').addClass(' delay-5s animated fadeInUp'); 
+			$('.intro-system-slide5 .animation-7').addClass(' delay-6s animated fadeInUp'); 
+			$('.intro-system-slide5 .animation-8').addClass(' delay-7s animated fadeInUp'); 
+});
+
+
+
+
+//Como buscar fornecedores...
+$( ".intro-system-button-to-slide-6" ).click(function() {
+			$('.intro-system-slide6 .animation-1').addClass(' animated fadeInUp'); 
+			$('.intro-system-slide6 .animation-2').addClass(' delay-1s animated fadeInUp'); 
+
+			$('.intro-system-slide6 .animation-3').addClass(' delay-2s animated fadeInUp'); 
+			$('.intro-system-slide6 .animation-4').addClass(' delay-3s animated fadeInUp'); 
+			$('.intro-system-slide6 .animation-5').addClass(' delay-3s animated fadeInUp');
+
+			$('.intro-system-slide6 .animation-6').addClass(' delay-4s animated fadeInUp'); 
+			$('.intro-system-slide6 .animation-7').addClass(' delay-5s animated fadeInUp'); 
+			$('.intro-system-slide6 .animation-8').addClass(' delay-5s animated fadeInUp'); 
+
+			$('.intro-system-slide6 .animation-9').addClass(' delay-6s animated fadeInUp'); 
+			$('.intro-system-slide6 .animation-10').addClass('delay-7s animated fadeInUp'); 
+			$('.intro-system-slide6 .animation-11').addClass('delay-7s animated fadeInUp'); 
+ });
+
+
+
+
+
+//Faça um checklist antes de começar!
+$( ".intro-system-button-to-slide-7" ).click(function() {
+			$('.intro-system-slide7 .animation-1').addClass(' animated fadeInUp'); 
+			$('.intro-system-slide7 .animation-2').addClass(' delay-1s animated fadeInUp'); 
+
+			$('.intro-system-slide7 .animation-3').addClass(' delay-2s animated fadeInUp'); 
+
+			$('.intro-system-slide7 .animation-4').addClass(' delay-3s animated fadeInUp'); 
+			$('.intro-system-slide7 .animation-5').addClass(' delay-4s animated fadeInUp'); 
+ });
+
+
+  
